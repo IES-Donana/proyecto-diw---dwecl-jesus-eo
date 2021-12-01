b604fefb053fb3bcde7bf6a74be1a54d7ac42109
Jesus Manuel Espinar Ocaña <jesusmanuel.espinar@iesdonana.org>
2021-12-02 00:05:48 +0100

    menu burguer

--- proyecto.js
+++ proyecto.js
@@ -1,9 +1,9 @@
 let btnenviar = document.getElementById("btnenviar");
-btnenviar.addEventListener('click',validar);
-
+let formulario = document.getElementById("main-contact-right");
 let nombre = document.querySelector('#nombre');
 let apellido = document.getElementById("apellido");
 let email = document.getElementById("email");
+btnenviar.addEventListener('click',validar);
 /*Cuando pulsamos una tecla comprueba si es el shift*/
 email.addEventListener('keydown', function (e) {
     if(e.shiftKey){
@@ -17,31 +17,65 @@ nombre.addEventListener('input',validar);
 apellido.addEventListener('input',validar);  */
 
 
-/* Hay que vaciar el valor de setCustomValidity porque se queda guardado la cadena introducida anteriormente */
+/* Hay que vaciar el valor de todos los elementos del formulario con setCustomValidity porque se queda guardado la cadena introducida anteriormente */
 function limpiarCustomvaliditi() {
-    setCustomValidity() = '';
+    for (const elemento of formulario.elements) {
+        elemento.setCustomValidity(" ");
+    }
 }
-
+//*********************Falla en el setcustomvalidity y en el pattron */
 function validarEmail() { 
     limpiarCustomvaliditi();
     //Si no contiene datos validos
     if(!email.checkValidity()){
+        console.log("Entra en valida email");
         if(email.validity.patternMismatch){
             email.setCustomValidity("Debes introducir un email valido");
+            return false;
         }
-        email.reportValidity();
+        //Si el campo email es vacio devuelve true
+        if(email.validity.valueMissing){
+            email.setCustomValidity("Debes rellenar el campo email");
         return false;
+        }
+        //Para mandar el mensaje creado
+        email.reportValidity();
     }
     return true;
 }
 
 function validarNombre() {
-    
+    limpiarCustomvaliditi();
+    if(!nombre.checkValidity()){
+        if(nombre.validity.valueMissing){
+            console.log("Entra en valida nombre");
+            nombre.setCustomValidity("Debes rellenar el campo nombre");
+            return false;
+        }
+        nombre.reportValidity();
+    }
+    return true;
 }
+function validarApellido() {
+    limpiarCustomvaliditi();
+    if(!apellido.checkValidity()){
+        if(apellido.validity.valueMissing){
+            console.log("Entra en valida apellido");
+            apellido.setCustomValidity("Debes rellenar el campo apellido");
+            return false;
+        }
+        
+    } 
+    apellido.reportValidity();
+    return true;
+}
+
 function validar(e) {
-    if(validarNombre() && validarEmail()) {
+    if(validarEmail() && validarNombre() &&  validarApellido()) {
+        console.log("Entra en validar todo true");
         document.getElementById('textarea').disabled = false;
     } else {
+        console.log("Entra en prevent");
         e.preventDefault();
     }
 }
